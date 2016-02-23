033b03a5b1dcf88baa69ce5e92e993c0f6ff352b
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-02-24 02:58:31 +0900

    fixed truncate

--- lib/phoenix_html_simplified_helpers/truncate.ex
+++ lib/phoenix_html_simplified_helpers/truncate.ex
@@ -2,9 +2,26 @@ defmodule Phoenix.HTML.SimplifiedHelpers.Truncate do
 
   def truncate(text, options \\ []) do
     len = options[:length] || 30
-    IO.inspect len
-    IO.inspect text
-    text
+    omi = options[:omission] || "..."
+
+    len_with_omi = len - String.length(omi)
+    stop =
+      if options[:separator] do
+        rindex(text, options[:separator]) || len_with_omi
+      else
+        len_with_omi
+      end
+
+    "#{String.slice(text, 0, stop)}#{omi}"
+  end
+
+  defp rindex(text, str), do: rindex(text, str, 0)
+  defp rindex(text, str, offset) do
+    {index, _} =
+      String.reverse(text)
+      |> :binary.match(str)
+
+    String.length(text) - index + offset
   end
 
 end
