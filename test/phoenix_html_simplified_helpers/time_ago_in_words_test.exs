ca4ba12aa1c0ef4c1be6b9fbb9fa0054454ad567
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-03-01 11:42:09 +0900

    Additional testing

--- test/phoenix_html_simplified_helpers/time_ago_in_words_test.exs
+++ test/phoenix_html_simplified_helpers/time_ago_in_words_test.exs
@@ -18,4 +18,34 @@ defmodule Phoenix.HTML.SimplifiedHelpers.TimeAgoInWordsTest do
     assert time_ago_in_words(Ecto.DateTime.utc) == "less than 5 seconds"
   end
 
+  test "time_ago_in_words normally in Japanese" do
+    Gettext.put_locale(Phoenix.HTML.SimplifiedHelpers.Gettext, "ja")
+    assert time_ago_in_words(Timex.Date.now) == "5秒以内"
+  end
+
+  test "time_ago_in_words epoch in Japanese" do
+    Gettext.put_locale(Phoenix.HTML.SimplifiedHelpers.Gettext, "ja")
+    assert time_ago_in_words(Timex.Date.now(:secs)) == "5秒以内"
+  end
+
+  test "time_ago_in_words with ecto in Japanese" do
+    Gettext.put_locale(Phoenix.HTML.SimplifiedHelpers.Gettext, "ja")
+    assert time_ago_in_words(Ecto.DateTime.utc) == "5秒以内"
+  end
+
+  test "time_ago_in_words normally in Español" do
+    Gettext.put_locale(Phoenix.HTML.SimplifiedHelpers.Gettext, "es")
+    assert time_ago_in_words(Timex.Date.now) == "menos de 5 segundos"
+  end
+
+  test "time_ago_in_words epoch in Español" do
+    Gettext.put_locale(Phoenix.HTML.SimplifiedHelpers.Gettext, "es")
+    assert time_ago_in_words(Timex.Date.now(:secs)) == "menos de 5 segundos"
+  end
+
+  test "time_ago_in_words with ecto in Español" do
+    Gettext.put_locale(Phoenix.HTML.SimplifiedHelpers.Gettext, "es")
+    assert time_ago_in_words(Ecto.DateTime.utc) == "menos de 5 segundos"
+  end
+
 end
