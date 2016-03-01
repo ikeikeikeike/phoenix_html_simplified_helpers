4b88951d7d2992c83c482ee5fd66be25df18eb41
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-03-01 13:32:54 +0900

    Bump version

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Phoenix.HTML.SimplifiedHelpers.Mixfile do
 
   def project do
     [app: :phoenix_html_simplified_helpers,
-     version: "0.2.7",
+     version: "0.2.8",
      elixir: ">= 1.0.0",
      source_url: "https://github.com/ikeikeikeike/phoenix_html_simplified_helpers",
      compilers: [:gettext] ++ Mix.compilers,
