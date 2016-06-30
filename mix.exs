5892cd7b6b3204ee536c6b731bdb47a4e5a7ecbd
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-06-30 14:55:55 +0900

    bump version

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Phoenix.HTML.SimplifiedHelpers.Mixfile do
 
   def project do
     [app: :phoenix_html_simplified_helpers,
-     version: "0.3.3",
+     version: "0.4.0",
      elixir: ">= 1.0.0",
      source_url: "https://github.com/ikeikeikeike/phoenix_html_simplified_helpers",
      compilers: [:gettext] ++ Mix.compilers,
@@ -35,10 +35,10 @@ defmodule Phoenix.HTML.SimplifiedHelpers.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:timex, "~> 2.1"},
-      {:timex_ecto, "~> 1.0"},
-      {:ecto, "~> 1.1"},
-      {:gettext, "~> 0.10"},
+      {:timex, "~> 2.1 or ~> 2.2"},
+      {:timex_ecto, "~> 1.0 or ~> 1.1 "},
+      {:ecto, "~> 1.1 or ~> 2.0"},
+      {:gettext, "~> 0.10 or ~> 0.11"},
       {:earmark, ">= 0.0.0", only: :dev},
       {:ex_doc, "~> 0.10", only: :dev},
       {:inch_ex, only: :docs},
